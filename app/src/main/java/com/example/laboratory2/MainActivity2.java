a88e190d41ac1d69643812cbf2fb81ac17225f66
Novikova Anastasia <novikova29292828@gmail.com>
2024-10-23 21:10:08 +0300

    lab2

--- app/src/main/java/com/example/laboratory2/MainActivity2.java
+++ app/src/main/java/com/example/laboratory2/MainActivity2.java
@@ -1,13 +1,7 @@
 package com.example.laboratory2;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
     @Override
--- app/src/main/java/com/example/laboratory2/MainActivity3.java
+++ app/src/main/java/com/example/laboratory2/MainActivity3.java
@@ -2,11 +2,7 @@ package com.example.laboratory2;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
     @Override
